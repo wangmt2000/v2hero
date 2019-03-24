4793934f097ae2dcc489fac03ca9028603fb43a5
wangmt2000 <32374305+wangmt2000@users.noreply.github.com>
2019-03-25 00:44:36 +0800

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-cd /v2raybin
-
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
-
-./v2ray
